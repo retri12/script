540a5882645ccc0d1bb340c8da117f70ed277337
retri12 <retrivpnstore@gmail.com>
2024-06-09 06:43:36 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -1297,9 +1297,9 @@ pip3 install telegram-send
 echo "LABEL=/boot /boot ext2 default, ro 1 2" >> /etc/
 clear
 echo ""
-echo "Kunjungi T.me/VnzVPNNOTIF_BOT "
+echo "Kunjungi t.me/Retrivpnbot "
 echo "masukan kode di bawah ke bot tele di atas"
-printf "6786147163:AAGNNXdGlN4PZGSdEH0ceRhxTzIZlmIjCf4" | telegram-send --configure
+printf "6982808580:AAFsgGVlVVG4wgIm8LJk8tIAEnxNnlm2kTo" | telegram-send --configure
 clear
 
 # // Menghapus File Installasj
